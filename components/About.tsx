e3143abc65c83a97d2822fa27021ec8331b82490
lmachens <leon@machens.koeln>
2022-08-13 21:30:37 +0200

    ✨ Add links to my other projects

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import styles from '../styles/About.module.css';
 
@@ -14,6 +15,40 @@ export default function About() {
           Leon Machens
         </a>{' '}
         | No Cross-Site trackers and other creepy things
+      </small>
+      <small>
+        More Projects:{' '}
+        <a
+          href="https://www.aeternum-map.gg/"
+          title="Interactive map for New World"
+          target="_blank"
+        >
+          Aeternum Map
+        </a>{' '}
+        |{' '}
+        <a
+          href="https://www.arkesia.gg/"
+          title="Interactive map for Lost Ark"
+          target="_blank"
+        >
+          Arkesia.gg
+        </a>{' '}
+        |{' '}
+        <a
+          href="https://www.soc.gg/"
+          title="A Songs of Conquest fansite"
+          target="_blank"
+        >
+          SoC.gg
+        </a>{' '}
+        |{' '}
+        <a
+          href="https://github.com/lmachens/skeleton"
+          title="Simply display any website as customizable Overlay"
+          target="_blank"
+        >
+          Skeleton
+        </a>
       </small>
     </footer>
   );
