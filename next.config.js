734ea6fe90215acfc15936410b2442b89d9f4003
lmachens <leon@machens.koeln>
2023-04-20 13:33:13 +0200

    Add ads.txt redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://api.nitropay.com/v1/ads-1487.txt',
+        permanent: true,
+      },
+    ];
+  },
 };
