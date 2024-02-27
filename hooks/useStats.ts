dc93a18f039524e8b4f921e89cb203aa0f8c5b81
lmachens <leon@machens.koeln>
2024-02-27 20:37:03 +0100

    Update URLs

--- hooks/useStats.ts
+++ hooks/useStats.ts
@@ -4,13 +4,11 @@ import { useEffect } from 'react';
 let plausible: ReturnType<typeof Plausible> | null = null;
 const initPlausible = () => {
   if (
-    typeof process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN === 'string' &&
-    typeof process.env.NEXT_PUBLIC_PLAUSIBLE_HOST === 'string' &&
     !plausible
   ) {
     plausible = Plausible({
-      domain: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN,
-      apiHost: process.env.NEXT_PUBLIC_PLAUSIBLE_HOST,
+      domain: "lol.th.gl",
+      apiHost: "https://metrics.th.gl",
     });
     plausible.enableAutoPageviews();
   }
